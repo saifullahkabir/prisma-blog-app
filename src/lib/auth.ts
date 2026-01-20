e5681636308fddae4334c27ed14a9221486229bf
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-01-21 02:25:45 +0600

    Sign Up & Sign In with Google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -201,4 +201,11 @@ export const auth = betterAuth({
       }
     },
   },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
