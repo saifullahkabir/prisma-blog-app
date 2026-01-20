5159b84e266c26fc6a460f6cdf18324f81e8c511
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-01-20 18:40:28 +0600

    Better Auth Setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { postRouter } from "./modules/post/post.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 
+app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use(express.json());
 
 app.use("/posts", postRouter);
