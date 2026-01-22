fb2ffb077a20e993129ed08e5442e8a0c04ac969
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-01-23 02:42:32 +0600

    Advanced Search Using OR & HAS Conditions

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -18,10 +18,25 @@ const createPost = async (
 const getAllPost = async (payload: { search: string | undefined }) => {
   const result = await prisma.post.findMany({
     where: {
-      title: {
-        contains: payload.search as string,
-        mode: "insensitive",
-      },
+      OR: [
+        {
+          title: {
+            contains: payload.search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+          content: {
+            contains: payload.search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+          tags: {
+            has: payload.search as string,
+          },
+        },
+      ],
     },
   });
   return result;
