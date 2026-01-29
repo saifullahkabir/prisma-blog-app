c843ef9514b4991f8ccc1df8bf4db75019ccbf41
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-01-29 22:26:31 +0600

    implement comments count and ordering

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -114,6 +114,11 @@ const getAllPost = async ({
     orderBy: {
       [sortBy]: sortOrder,
     },
+    include: {
+      _count: {
+        select: { comments: true },
+      },
+    },
   });
 
   const total = await prisma.post.count({
@@ -156,20 +161,26 @@ const getPostById = async (postId: string) => {
             parentId: null,
             status: CommentStatus.APPROVED,
           },
+          orderBy: { createdAt: "desc" },
           include: {
             replies: {
               where: {
                 status: CommentStatus.APPROVED,
               },
+              orderBy: { createdAt: "asc" },
               include: {
                 replies: {
                   where: {
                     status: CommentStatus.APPROVED,
                   },
+                  orderBy: { createdAt: "asc" },
                 },
               },
             },
           },
+        },
+        _count: {
+          select: { comments: true },
         },
       },
     });
